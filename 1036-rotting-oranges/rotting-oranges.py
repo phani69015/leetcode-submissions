188d46c2bc99eaaa80867079cf5584e2f5dcfe15
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-25 20:23:03 +0530

    Time: 3 ms (74.87%) | Memory: 19.3 MB (40.89%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,19 +2,17 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        fresh = 0
         mins = 0
+        fresh = 0
         m = len(grid)
         n = len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j]==2:
                     q.append((i,j))
                 elif grid[i][j]==1:
                     fresh+=1
-                else:
-                    continue 
-        dir = {
+        d = {
             (0,1),
             (1,0),
             (-1,0),
@@ -23,16 +21,19 @@ class Solution:
         while q:
             level = len(q)
             for _ in range(level):
-                i,j = q.popleft()
-                for x,y in dir:
-                    xi = i+x
-                    yi = j+y
-                    if 0 <= xi < m and 0<=yi<n:
-                        if grid[xi][yi]==1:
-                            fresh -= 1
-                            grid[xi][yi] = 2
-                            q.append((xi,yi)) 
+                x,y = q.popleft()
+                for i,j in d:
+                    xi = x+i
+                    yj = y+j
+                    if 0<=xi<m and 0<=yj<n and grid[xi][yj]==1:
+                        grid[xi][yj] = 2
+                        fresh -= 1
+                        q.append((xi,yj))
             if q:
-                mins += 1 
-        return mins if fresh==0 else -1
+                mins+=1
+        
+        return mins if fresh ==0 else -1
+
+
+
         
