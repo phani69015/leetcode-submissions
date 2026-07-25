85b43de39b898498e561979ee60608b8a699aff6
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-25 20:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (84.76%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if image[sr][sc]==color:
@@ -5,38 +6,41 @@ class Solution:
         rows = len(image)
         cols = len(image[0])
 
-        original = image[sr][sc]
-
-        # If the color is already the same, nothing to do.
-        if original == color:
-            return image
-
         q = deque()
-        q.append((sr, sc))
-        image[sr][sc] = color   # mark as visited by changing the color
+        q.append((sr,sc))
+        curr = image[sr][sc]
+        image[sr][sc]=color
 
-        directions = [
-            (-1, 0),  # Up
-            (1, 0),   # Down
-            (0, -1),  # Left
-            (0, 1)    # Right
-        ]
+        d = {
+            (0,1),
+            (1,0),
+            (0,-1),
+            (-1,0)
+        }
 
         while q:
-            r, c = q.popleft()
+            i,j = q.popleft()
+            for x,y in d:
+                xi = x + i
+                yj = y + j
 
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-
-                # Check boundaries
-                if 0 <= nr < rows and 0 <= nc < cols:
-                    # Visit only cells with the original color
-                    if image[nr][nc] == original:
-                        image[nr][nc] = color
-                        q.append((nr, nc))
+                if 0<=xi<rows and 0<=yj<cols and image[xi][yj]==curr:
+                    image[xi][yj]=color
+                    q.append((xi,yj))
 
         return image
+
+
+
+
+
+        
+
+
+
+
+
+
 
         
 
