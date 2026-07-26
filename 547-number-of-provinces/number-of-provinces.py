ae6c5ffeb40246803a9506bcfed5b6e864132652
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-26 16:07:03 +0530

    Time: 7 ms (47.71%) | Memory: 20.4 MB (69.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,55 +1,39 @@
 from collections import deque
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
-        # m = len(grid)
-        # n = len(grid[0])
+        #approach 1 -> convert to adjaceny list and find no of components
+        def matrix_to_list(matrix):
+            adj_list = []
+            
+            for i in range(len(matrix)):
+                neighbors = []
+                for j in range(len(matrix[i])):
+                    if matrix[i][j] == 1:
+                        neighbors.append(j)
+                adj_list.append(neighbors)
+                
+            return adj_list
 
-        # vis = [[0 for _ in range(n)]for _ in range(m)]
+        adj = matrix_to_list(grid)
 
-        # d = {
-        #     (1,0),
-        #     (0,1),
-        #     (-1,0),
-        #     (0,-1)
-        # }
-
-        # q = deque()
-        # count = 0
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         if vis[i][j]==0 and grid[i][j]==1:
-        #             count+=1
-        #             q.append((i,j))
-        #             vis[i][j]=1
-        #             while q:
-        #                 u,v = q.popleft()
-        #                 for x,y in d:
-        #                     xu = x+u
-        #                     yv = y+v
-
-        #                     if 0<=xu<m and 0<=yv<n and vis[xu][yv]==0 and grid[xu][yv]==1:
-        #                         q.append((xu,yv))
-        #                         vis[xu][yv]=1
-        # return count
-
-        vis = [False]*len(grid)
-        n = len(grid)
-        count = 0
+        v = [False for _ in range(len(adj))]
+        c = 0
         q = deque()
 
-        for i in range(len(vis)):
-            if not vis[i]:
-                count+=1
-                vis[i]=True
+        for i in range(len(adj)):
+            if not v[i]:
+                c+=1
                 q.append(i)
+                v[i]=True
+
                 while q:
-                    city = q.popleft()
-                    for nei in range(len(grid[city])):
-                        if grid[city][nei]==1  and not vis[nei]:
-                            vis[nei]=True
-                            q.append(nei) 
-        return count
+                    key = q.popleft()
+                    for nei in adj[key]:
+                        if not v[nei]:
+                            v[nei]=True
+                            q.append(nei)
+        return c
+
 
 
 
