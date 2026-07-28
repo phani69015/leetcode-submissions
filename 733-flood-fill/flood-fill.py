7f2ff5cf4196747375e07a135cc961f09f327dcf
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-28 21:58:07 +0530

    Time: 2 ms (17.63%) | Memory: 19.6 MB (44.44%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -6,27 +6,56 @@ class Solution:
         rows = len(image)
         cols = len(image[0])
 
+        #bfs
+        # curr = image[sr][sc]
+        # image[sr][sc]=color
+
+        # q = deque()
+        # q.append((sr,sc))
+
+        # d = {
+        #     (-1,0),
+        #     (0,-1),
+        #     (1,0),
+        #     (0,1)
+        # }
+
+        # while q:
+        #     a,b = q.popleft()
+        #     for x,y in d:
+        #         ax,by = a+x , b+y
+        #         if 0<=ax<rows and 0<=by<cols and image[ax][by]==curr:
+        #             q.append((ax,by))
+        #             image[ax][by]=color 
+        # return image
+
+        #dfs 
+
         curr = image[sr][sc]
-        image[sr][sc]=color
 
-        q = deque()
-        q.append((sr,sc))
-
-        d = {
-            (-1,0),
-            (0,-1),
-            (1,0),
-            (0,1)
-        }
-
-        while q:
-            a,b = q.popleft()
+        def dfs(a,b):
+            d = {
+                (-1,0),
+                (0,-1),
+                (1,0),
+                (0,1)
+            }
+            image[a][b]=color
             for x,y in d:
                 ax,by = a+x , b+y
                 if 0<=ax<rows and 0<=by<cols and image[ax][by]==curr:
-                    q.append((ax,by))
-                    image[ax][by]=color 
+                    dfs(ax,by)
+            
+        dfs(sr,sc)
+
         return image
+
+
+
+
+            
+
+
 
         
             
