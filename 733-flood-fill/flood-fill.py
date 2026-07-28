dd744da27bed409018e24a9931e7f275339d6a2a
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-28 21:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (84.86%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -6,29 +6,32 @@ class Solution:
         rows = len(image)
         cols = len(image[0])
 
-        q = deque()
-        q.append((sr,sc))
         curr = image[sr][sc]
         image[sr][sc]=color
 
+        q = deque()
+        q.append((sr,sc))
+
         d = {
-            (0,1),
-            (1,0),
+            (-1,0),
             (0,-1),
-            (-1,0)
+            (1,0),
+            (0,1)
         }
 
         while q:
-            i,j = q.popleft()
+            a,b = q.popleft()
             for x,y in d:
-                xi = x + i
-                yj = y + j
-
-                if 0<=xi<rows and 0<=yj<cols and image[xi][yj]==curr:
-                    image[xi][yj]=color
-                    q.append((xi,yj))
-
+                ax,by = a+x , b+y
+                if 0<=ax<rows and 0<=by<cols and image[ax][by]==curr:
+                    q.append((ax,by))
+                    image[ax][by]=color 
         return image
+
+        
+            
+
+
 
 
 
