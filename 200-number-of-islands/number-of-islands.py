1d8eaad16e3ce06014f0fbf373af1517ec853dc8
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-28 22:06:41 +0530

    Time: 246 ms (51.11%) | Memory: 21.6 MB (47.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,34 +2,38 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-
-        vis = [[0 for i in range(n)]for _ in range(m)]
+        #bfs
+        vis = [[False for _ in range(n)] for _ in range(m)]
 
         d = {
-            (1,0),
             (0,1),
-            (-1,0),
-            (0,-1)
+            (0,-1),
+            (1,0),
+            (-1,0)
         }
 
         q = deque()
-        count = 0
+        c = 0
 
         for i in range(m):
             for j in range(n):
-                if vis[i][j]==0 and grid[i][j]=='1':
-                    count+=1
+                if grid[i][j]=='1' and not vis[i][j]:
+                    c+=1
                     q.append((i,j))
-                    vis[i][j]=1
+                    vis[i][j]=True
                     while q:
-                        u,v = q.popleft()
+                        a,b = q.popleft()
                         for x,y in d:
-                            ux = u+x
-                            vy = v+y
-                            if 0<=ux<m and 0<=vy<n and grid[ux][vy]=='1' and vis[ux][vy]==0:
-                                q.append((ux,vy))
-                                vis[ux][vy]=1
-        return count
+                            ax , by = a+x , b+y
+                            if 0<=ax<m and 0<=by<n and grid[ax][by]=='1' and not vis[ax][by]:
+                                q.append((ax,by))
+                                vis[ax][by]=True 
+        return c
+
+
+
+
+
 
 
 
