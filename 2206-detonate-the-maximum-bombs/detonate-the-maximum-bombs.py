ed0a2e4278d7dec4caac6aa19124d40a64b6e642
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-08-07 23:17:50 +0530

    Time: 237 ms (91.90%) | Memory: 19.6 MB (64.53%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -5,7 +5,7 @@ class Solution:
     def distance(self, arr1, arr2):
         ps = arr2[1] - arr1[1]
         pf = arr2[0] - arr1[0]
-        return ps * ps + pf * pf
+        return math.sqrt(ps * ps + pf * pf)
 
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         m = len(bombs)
@@ -15,24 +15,44 @@ class Solution:
             for j in range(i + 1, m):
                 dist = self.distance(bombs[i], bombs[j])
 
-                if dist <= bombs[i][2] ** 2:
+                if dist <= bombs[i][2]:
                     adj[i].append(j)
 
-                if dist <= bombs[j][2] ** 2:
+                if dist <= bombs[j][2]:
                     adj[j].append(i)
 
-        def dfs(node, visited):
-            visited.add(node)
+        # def dfs(node, visited):
+        #     visited.add(node)
 
-            for nei in adj[node]:
-                if nei not in visited:
-                    dfs(nei, visited)
+        #     for nei in adj[node]:
+        #         if nei not in visited:
+        #             dfs(nei, visited)
+
+        # ans = 0
+
+        # for i in range(m):
+        #     visited = set()
+        #     dfs(i, visited)
+        #     ans = max(ans, len(visited))
+
+        # return ans
 
         ans = 0
-
-        for i in range(m):
-            visited = set()
-            dfs(i, visited)
-            ans = max(ans, len(visited))
+        for st in range(m):
+            vis = [False for _ in range(m)]
+            q = deque([st])
+            vis[st]=True
+            c = 0
+            while q:
+                key = q.popleft()
+                c+=1
+                for nei in adj[key]:
+                    if not vis[nei]:
+                        vis[nei]=True
+                        q.append(nei)
+            ans = max(ans,c)
 
         return ans
+        
+
+
