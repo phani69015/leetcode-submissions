3db7091e0f358e91956637983516b7f046b7febf
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-08-07 23:51:26 +0530

    Time: 71 ms (58.64%) | Memory: 21.8 MB (28.09%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,47 +1,27 @@
-
-from collections import deque
-from typing import List
-
 class Solution:
-    def findAllRecipes(
-        self,
-        recipes: List[str],
-        ingredients: List[List[str]],
-        supplies: List[str]
-    ) -> List[str]:
-
-        ind = {}
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         adj = {}
+        ind = {}
 
-        # Build graph
         for i in range(len(recipes)):
-            recipe = recipes[i]
-            ind[recipe] = len(ingredients[i])
+            r = recipes[i]
+            ind[r]=len(ingredients[i])
 
-            for ingredient in ingredients[i]:
-                if ingredient not in adj:
-                    adj[ingredient] = []
+            for ing in ingredients[i]:
+                if ing not in adj:
+                    adj[ing]=[]
+                adj[ing].append(r)
 
-                adj[ingredient].append(recipe)
-
-        # Initially available ingredients
         q = deque(supplies)
-
         ans = []
-
-        # Topological sort
         while q:
-            item = q.popleft()
-
-            # If this item is a recipe, we successfully created it
-            if item in ind:
-                ans.append(item)
-
-            # This item may help unlock some recipes
-            for recipe in adj.get(item, []):
-                ind[recipe] -= 1
-
-                if ind[recipe] == 0:
-                    q.append(recipe)
-
+            key = q.popleft()
+            if key in ind:
+                ans.append(key)
+            for nei in adj.get(key,[]):
+                ind[nei]-=1
+                if ind[nei]==0:
+                    q.append(nei)
         return ans
+
+
