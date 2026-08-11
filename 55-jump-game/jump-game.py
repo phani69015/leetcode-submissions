dcfa2ff14b71ce2ab354f5c91fb8f7f27faa2a88
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-08-11 12:51:22 +0530

    Time: 7 ms (98.26%) | Memory: 20.1 MB (84.77%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -8,4 +8,31 @@ class Solution:
             if nums[i]>gas:
                 gas = nums[i]
         return True
+
+        #plain dfs
+
+        # def dfs(i):
+        #     if i >= len(nums)-1:
+        #         return True
+        #     for jump in range(1,nums[i]+1):
+        #         if dfs(i+jump):
+        #             return True
+        #     return False
+        # return dfs(0)
+
+        #dfs with memoization
+        # memo = {}
+        # def dfs(i):
+        #     if i >= len(nums)-1:
+        #         return True
+        #     if i in memo:
+        #         return memo[i]
+        #     for jump in range(1,nums[i]+1):
+        #         if dfs(i+jump):
+        #             memo[i]=True
+        #             return True
+        #     memo[i]=False
+        #     return False
+        # return dfs(0)
+
         
